04710283ff1b4a57999c522f8a2128bc1639e7c6
Chris Dent <chris.dent@gmail.com>
2014-04-11 15:15:55 +0100

    add required DJANGO_SETTINGS_MODULES env

--- main.py
+++ main.py
@@ -2,7 +2,13 @@
 main.py provides the app callable to the AppEngine.
 """
 
+import os
+
 from django.core.handlers.wsgi import WSGIHandler
+
+
+# This _must_ be set somewhere and here seems to work.
+os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 
 app = WSGIHandler()
